2bf431785639902e94aec606a14d67a10e4021fc
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2022-06-23 09:45:35 +0900

    feat: installed pg, aws-3s upload provider, update config

--- config/database.js
+++ config/database.js
@@ -7,7 +7,6 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'strapi'),
       user: env('DATABASE_USERNAME', ''),
       password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
